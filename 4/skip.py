045b385029623c4a3d59eed42e7f8c1325474414
mjagintowicz <102296275+mjagintowicz@users.noreply.github.com>
2023-04-02 16:51:27 +0200

    chyba działa
    🌴 🦮

--- 4/skip.py
+++ 4/skip.py
@@ -1,4 +1,4 @@
-# nieskonczone
+# skonczone
 import random
 
 
@@ -29,61 +29,49 @@ class SkipList:
         elem = self.head
         for level in reversed(range(self.head.height)):
             while elem.tab[level] is not None and elem.tab[level].key <= key:
-                if elem.tab[level].key < key:  # jeśli tak to przejdź na następny element
+                if elem.tab[level].key < key:
                     elem = elem.tab[level]
-                elif elem.tab[level].key > key:  # jeśli tak jest to dodaj ten node do listy i idź level niżej
-                    break  # czyli wyjdź z while
-                else:  # kiedy są równe, to po prostu podmień
+                elif elem.tab[level].key > key:
+                    break
+                else:
                     return elem.tab[level].value
         return None
 
-    # insert działa, jak mam dopisywać elementy na sam koniec w miarę
-    # nadpisywanie chyba też
     def insert(self, key, data):
         elem = self.head
         p = []
         inserted = False
         for level in reversed(range(self.head.height)):
-            if elem == self.head and elem.tab[level] is None:
+            if elem.tab[level] is None:
                 p.append(elem)
+                continue
             while elem.tab[level] is not None:
-                if elem.tab[level].key < key:  # jeśli tak to przejdź na następny element
+                if elem.tab[level].key < key:
                     elem = elem.tab[level]
                     if elem.tab[level] is None:
                         p.append(elem)
                         break
-                elif elem.tab[level].key > key:  # jeśli tak jest to dodaj ten node do listy i idź level niżej
+                elif elem.tab[level].key > key:
                     p.append(elem)
                     break
-                else:  # kiedy są równe, to po prostu podmień
+                else:
                     elem.tab[level].value = data
                     inserted = True
                     break
             if inserted:
                 break
-        if not inserted and p:
+        if not inserted:
             new_elem = Elem(key, data)
             if new_elem.height > self.head.height:
                 new_elem.height = self.head.height
-            it = 0
-            clvl = -1
-            while p:
+            for lvl in range(new_elem.height):
                 prev = p.pop()
-                clvl += 1
-                if clvl < it:
-                    clvl = it
-                for lvl in range(clvl, prev.height):
-                    if it >= new_elem.height:
-                        break
-                    new_elem.tab[lvl] = prev.tab[lvl]  # teraz nowy element wskazuje na to, co jego poprzednik
-                    prev.tab[lvl] = new_elem  # a ten poprzednik wskazuje na nowy element
-                    it += 1
-                if it >= new_elem.height:
-                    break
+                new_elem.tab[lvl] = prev.tab[lvl]
+                prev.tab[lvl] = new_elem
 
     def display_list_(self):
-        node = self.head.tab[0]  # pierwszy element na poziomie 0
-        keys = []  # lista kluczy na tym poziomie
+        node = self.head.tab[0]
+        keys = []
         while node is not None:
             keys.append(node.key)
             node = node.tab[0]
@@ -122,7 +110,7 @@ class SkipList:
                 prev = p.pop()
                 prev.tab[lvl] = found_elem.tab[lvl]
         else:
-            return None  # brak klucza
+            return None
 
     def __str__(self):
         elem = self.head
@@ -140,34 +128,50 @@ class SkipList:
 
 
 def main():
-    lvl = random_level(0.5)
-    skip = SkipList(lvl)
+    skip = SkipList(random_level(0.5))
+
     for it in range(1, 16):
         skip.insert(it, chr(it+64))
+
     print(skip)
-    skip.display_list_()
 
     print(skip.search(2))
 
     skip.insert(2, 'Z')
-    skip.display_list_()
 
     print(skip.search(2))
 
     skip.remove(5)
-    skip.display_list_()
     skip.remove(6)
-    skip.display_list_()
     skip.remove(7)
-    skip.display_list_()
 
     print(skip)
 
     skip.insert(6, 'W')
+
     print(skip)
+
+    reverse_skip = SkipList(random_level(0.5))
+
+    for it in reversed(range(1, 16)):
+        reverse_skip.insert(it, chr(it + 64))
+    print(reverse_skip)
+
+    print(reverse_skip.search(2))
+
+    reverse_skip.insert(2, 'Z')
+
+    print(reverse_skip.search(2))
+
+    reverse_skip.remove(5)
+    reverse_skip.remove(6)
+    reverse_skip.remove(7)
+
+    print(reverse_skip)
+
+    reverse_skip.insert(6, 'W')
+
+    print(reverse_skip)
 
 
 main()
-
-
-
